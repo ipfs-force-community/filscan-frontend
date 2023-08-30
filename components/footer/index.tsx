ab9d133f1fe99bd11778b40f9511920683fc1229
jinming.liu <mingming.lucky@foxmail.com>
2023-08-30 18:58:38 +0800

    feat: update footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,12 +1,60 @@
 /** @format */
 
 import { Layout } from 'antd';
+import logo from '@/assets/images/logo.svg'
+import Image from 'next/image'
+import { Translation } from '../hooks/Translation';
+import { getSvgIcon } from '@/svgsIcon';
 
 const { Footer, Header } = Layout;
 
+const footerLinks = [
+  {
+    label: "twitter",
+    type:'_blank',
+    link:'https://twitter.com/FilscanOfficial'
+  },
+  {
+    label: "telegram",
+    type:'_blank',
+    link:'https://t.me/+bI9fUEkmPjMzYjg1'
+  },
+  {
+    label: "outlook",
+    type:'_self',
+    link:'mailto:filscan@ipfsforce.com'
+  }
+]
+
 export default () => {
+  const { tr } = Translation({ ns: 'common' });
+
   console.log('====eee');
   return (
-    <Footer className='footer !bg-black h-[200px]'>{/* <Image /> */}</Footer>
+    <Footer className='footer !bg-black h-[140px] flex justify-between flex-col'>
+      <span className='!text-white flex items-center'>
+        <Image src={logo} width={45} height={45} alt='logo' />
+        <span className='font-Barlow font-medium text-xl ml-2'>Filscan.io </span>
+      </span>
+      <div className='flex justify-between text-border_des'>
+        <div className='flex flex-col gap-y-1 text-xs'>
+          <span>
+            { tr('footer_des1')}
+          </span>
+          <span>
+            { tr('footer_des2')}
+          </span>
+        </div>
+        <ul className='flex text-white gap-x-5'>
+          {footerLinks.map(linkItem => {
+            return <a key={linkItem.label}
+              className='w-5 h-5 bg-white rounded'
+              target={linkItem.type}
+              style={{ color: '#fff' }} href={linkItem.link}>{
+                getSvgIcon(linkItem.label)}</a>
+          })}
+        </ul>
+      </div>
+    </Footer>
   );
 };
--- i18n/zh/common.js
+++ i18n/zh/common.js
@@ -1,59 +1,58 @@
 const common = {
-      zh: '中',
-        en: 'En',
-        kr:'Kr',
+  zh: '中',
+  en: 'En',
+  kr:'Kr',
   login: '登录',
   cancel: '取消',
-    confirm:'确认',
-    lang:'中',
-    fil: 'FIL',
-    last_time:'最新区块时间',
-    base_fee: 'Base Fee',
-    last_height: '最新区块高度',
-    go_home:'返回首页',
-    go_login:'去登录',
-     //search 
-    'search_holder':'请输入地址/消息ID/高度/区块Cid/节点ID/FNS',
-    all: '全部筛选类型',
-    address: '地址',
-    message_id: '消息ID',
-    height: '高度',
-    cid: '区块CID',
+  confirm:'确认',
+  lang:'中',
+  fil: 'FIL',
+  last_time:'最新区块时间',
+  base_fee: 'Base Fee',
+  last_height: '最新区块高度',
+  go_home:'返回首页',
+  go_login:'去登录',
+  //search
+  'search_holder':'请输入地址/消息ID/高度/区块Cid/节点ID/FNS',
+  all: '全部筛选类型',
+  address: '地址',
+  message_id: '消息ID',
+  height: '高度',
+  cid: '区块CID',
   node: '节点',
-    
 
   //messages
   no_account:'该邮箱未注册,请先注册',
 
-    //登录注册
-    register:'邮箱注册',
-    password_login: '密码登录',
-    verification_code: '验证码登录',
-    email_placeholder: '输入邮箱号',
-    password_placeholder: '输入密码',
-    code_placeholder: '输入验证码',
-    remember_me: '记住并自动登录',
-    forgot_password: '忘记密码',
-    get_code: '获取验证码',
-    retry_code:'重新发送',
-    no_account: '还没有账号?',
-    have_account:'已有账号?',
-    go_register: '立即注册',
-    new_password: '设置密码',
-    confirm_password: '确认密码',
-    agreement: '注册即代表同意《协议》、《隐私政策》',
-    
-    //校验
-    email_required: '请输入您的邮箱',
-    email_rules: '邮箱格式有误,请修改',
-    code_rules:'请正确输入6位数字验证码',
-    password_rules: '请输入 8-20 个字符，需同时包含数字、字母以及特殊符号,密码不能与您的邮箱地址相同',
-  confirm_password_rules: '两次输入的密码不一致，请重新输入',
-    email_exists:'邮箱已存在',
-  Export:'下载Excel'
+  //登录注册
+  register:'邮箱注册',
+  password_login: '密码登录',
+  verification_code: '验证码登录',
+  email_placeholder: '输入邮箱号',
+  password_placeholder: '输入密码',
+  code_placeholder: '输入验证码',
+  remember_me: '记住并自动登录',
+  forgot_password: '忘记密码',
+  get_code: '获取验证码',
+  retry_code:'重新发送',
+  no_account: '还没有账号?',
+  have_account:'已有账号?',
+  go_register: '立即注册',
+  new_password: '设置密码',
+  confirm_password: '确认密码',
+  agreement: '注册即代表同意《协议》、《隐私政策》',
 
-    
-    
-    
+  //校验
+  email_required: '请输入您的邮箱',
+  email_rules: '邮箱格式有误,请修改',
+  code_rules:'请正确输入6位数字验证码',
+  password_rules: '请输入 8-20 个字符，需同时包含数字、字母以及特殊符号,密码不能与您的邮箱地址相同',
+  confirm_password_rules: '两次输入的密码不一致，请重新输入',
+  email_exists:'邮箱已存在',
+  Export: '下载Excel',
+  //footer
+  'footer_des1':'Filecoin区块浏览器及数据服务平台，提供基于 Filecoin',
+  'footer_des2':'的各类收节点收益排行榜、区块链数据查询、可视化图表等一站式数据服务。',
+
 }
 export default common
