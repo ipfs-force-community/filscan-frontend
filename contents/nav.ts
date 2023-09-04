9b16021569e7e0f5f95e18de8ebb94054d28de6f
jinming.liu <mingming.lucky@foxmail.com>
2023-09-04 10:35:03 +0800

    feat: merge pre

--- contents/nav.ts
+++ contents/nav.ts
@@ -5,13 +5,96 @@ const navMenu: Array<Menu_Info> = [
     key: 'home',
     link: '/home'
   },
+  // {
+  //   key: 'tipset',
+  //   children: [
+  //     //   {
+  //     //       key: 'tipset_chain',
+  //     //       link:'/tipset/chain/'
+  //     //   },
+  //     {
+  //       key: 'tipset_message',
+  //       link: '/tipset/message-list/'
+  //     },
+  //     {
+  //       key: 'tipset_ranking',
+  //       link: '/tipset/address-list/'
+  //     },
+  //     {
+  //       key: 'tipset_dsn',
+  //       link: '/tipset/dsn/'
+  //     },
+  //     {
+  //       key: 'tipset_pool-message',
+  //       link: '/tipset/pool-message/'
+  //     },
+  //   ]
+  // },
+  // {
+  //   key: 'contract',
+  //   preIcon: 'newIcon',
+  //   color: '#F44C30',
+  //   children: [
+  //     {
+  //       key: 'token',
+  //       link: '/contract/token/'
+  //     },
+  //     {
+  //       key: 'nft',
+  //       link: '/contract/nft/'
+  //     },
+  //     {
+  //       key: 'defi_dashboard',
+  //       link: '/fevm/defi/'
+  //     },
+  //     {
+  //       key: 'contract_rank',
+  //       link: '/contract/rank/'
+  //     },
+  //     {
+  //       key: 'contract_list',
+  //       link: '/contract/list/'
+  //     },
+  //     {
+  //       key: 'contract_verify',
+  //       link: '/contract/verify/'
+  //     },
+
+  //   ]
+  // },
+  // { key: 'ranking', link: '/rank' },
+  // {
+  //   key: 'statistics',
+  //   children: [
+  //     { key: 'statistics_gas', link: '/statistics/gas' },
+  //     { key: 'statistics_base', link: '/statistics/power' },
+  //     { key: 'statistics_fil', link: '/statistics/fil' },
+  //     { key: 'statistics_charts', link: '/statistics/charts' },
+  //   ]
+  // },
+  // {
+  //   key: 'fvm',
+  //   sufIcon: 'hotIcon',
+  //   color: '#F44C30',
+  //   link: '/fvm'
+  // },
+  {
+    out_key: 'account',
+    key: 'account',
+    link: '/account'
+  }
+]
+
+const mobileNavMenu: Menu_Info[] = [
+  {
+    key: 'fvm',
+    sufIcon: 'hotIcon',
+    color: '#F44C30',
+    link: '/fvm'
+  },
   {
     key: 'tipset',
     children: [
-      //   {
-      //       key: 'tipset_chain',
-      //       link:'/tipset/chain/'
-      //   },
       {
         key: 'tipset_message',
         link: '/tipset/message-list/'
@@ -62,7 +145,6 @@ const navMenu: Array<Menu_Info> = [
 
     ]
   },
-  { key: 'ranking', link: '/rank' },
   {
     key: 'statistics',
     children: [
@@ -72,94 +154,12 @@ const navMenu: Array<Menu_Info> = [
       { key: 'statistics_charts', link: '/statistics/charts' },
     ]
   },
-  {
-    key: 'fvm',
-    sufIcon: 'hotIcon',
-    color: '#F44C30',
-    link: '/fvm'
-  },
-  {
-    out_key: 'account',
-    key: 'account',
-    link: '/account'
-  }
+  //   { key: 'ranking' ,link:'/rank'},
+  //   {
+  //     out_key: 'provider',
+  //     key:'provider',
+  //     outLink:'http://v1.filscan.io/account?key=login'
+  //   }
 ]
 
-const mobileNavMenu: Menu_Info[] = [
-  {
-    key: 'fvm',
-    sufIcon: 'hotIcon',
-    color:'#F44C30',
-    link:'/fvm'
-  },
-  {
-    key: 'tipset',
-    children: [
-      {
-        key: 'tipset_message',
-        link:'/tipset/message-list/'
-      },
-      {
-        key: 'tipset_ranking',
-        link:'/tipset/address-list/'
-      },
-      {
-        key: 'tipset_dsn',
-        link:'/tipset/dsn/'
-      },
-      {
-        key: 'tipset_pool-message',
-        link:'/tipset/pool-message/'
-      },
-    ]
-  },
-  {
-    key: 'contract',
-    preIcon: 'newIcon',
-    color:'#F44C30',
-    children: [
-      {
-        key: 'token',
-        link:'/contract/token/'
-      },
-      {
-        key: 'nft',
-        link:'/contract/nft/'
-      },
-      {
-        key: 'defi_dashboard',
-        link:'/fevm/defi/'
-      },
-      {
-        key: 'contract_rank',
-        link:'/contract/rank/'
-      },
-      {
-        key: 'contract_list',
-        link:'/contract/list/'
-      },
-      {
-        key: 'contract_verify',
-        link:'/contract/verify/'
-      },
-
-    ]
-  },
-  {
-    key: 'statistics',
-    children: [
-      { key: 'statistics_gas',link:'/statistics/gas'},
-      { key: 'statistics_base',link:'/statistics/power' },
-      { key: 'statistics_fil' ,link:'/statistics/fil'},
-      { key: 'statistics_charts', link: '/statistics/charts' },
-    ]
-  },
-//   { key: 'ranking' ,link:'/rank'},
-//   {
-//     out_key: 'provider',
-//     key:'provider',
-//     outLink:'http://v1.filscan.io/account?key=login'
-//   }
-]
-
-export { navMenu,mobileNavMenu }
+export { navMenu, mobileNavMenu }
