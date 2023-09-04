562a69a7cb4fa31d2be9b73663599cc323156c66
jinming.liu <mingming.lucky@foxmail.com>
2023-09-04 10:05:07 +0800

    feat: update

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -39,94 +39,49 @@ function Home(props: any) {
 
   return (
     <div className={styles['home-page']}>
-      <div className='main_contain !mt-[26px]'>
-        <MobileView>
-          <MobileBanner />
-        </MobileView>
+
+      <MobileView>
+        <MobileBanner />
+      </MobileView>
+      <div className='main_bg_color'>
         <BrowserView>
           <Banner />
         </BrowserView>
-        <div className='flex justify-between h-[270px] gap-x-5 meta-gas'>
-          <Meta />
-          <div className='border card_shadow w-[403px] h-[270px] rounded-xl border_color gas-wrap'>
-            <div className={`flex justify-between text-xs font-PingFang p-5`}>
-              {tr('base_gas')}
-              <Link href={`/statistics/gas/`}>
-                <Image
-                  className='cursor-pointer'
-                  src={go}
-                  width={18}
-                  height={18}
-                  alt='go'
-                />
-              </Link>
-            </div>
-            <Gas className={'w-full !h-[210px]'} />
-          </div>
-        </div>
-        <div className='flex justify-between mt-12 gap-x-5 h-[400px] box-column'>
-          <Trend origin='home' className={'w-full !h-full'} />
-          <Trend origin='home' className={'w-full !h-full'} />
-        </div>
-        <div className='mt-32' ref={ref}>
-          <Rank origin={'home'} />
-        </div>
-
         <BrowserView>
-          <div className='flex w-full mt-12 gap-x-5 h-[489px]  ' ref={ref1}>
-            <div className='flex-1'>
-              <div className={`flex justify-between items-center h-[30px]`}>
-                <div className='font-PingFang font-semibold text-lg	'>
-                  {tr('contract_rank')}
+          <div className='main_contain !mt-[26px]'>
+            <div className='flex justify-between h-[270px] gap-x-5 meta-gas'>
+              <Meta />
+              <div className='border card_shadow w-[403px] h-[270px] rounded-xl border_color gas-wrap'>
+                <div className={`flex justify-between text-xs font-PingFang p-5`}>
+                  {tr('base_gas')}
+                  <Link href={`/statistics/gas/`}>
+                    <Image
+                      className='cursor-pointer'
+                      src={go}
+                      width={18}
+                      height={18}
+                      alt='go'
+                    />
+                  </Link>
                 </div>
-                <Link href={`/contract/rank`}>
-                  <Image
-                    className='cursor-pointer'
-                    src={go}
-                    width={18}
-                    height={18}
-                    alt='go'
-                  />
-                </Link>
+                <Gas className={'w-full !h-[210px]'} />
               </div>
-              <Gas className={'w-full !h-[210px]'} />
+            </div>
+            <div className='flex justify-between mt-12 gap-x-5 h-[400px] box-column'>
+              <Trend origin='home' className={'w-full !h-full'} />
+              <Trend origin='home' className={'w-full !h-full'} />
+            </div>
+            <div className='mt-32' ref={ref}>
+              <Rank origin={'home'} />
             </div>
           </div>
-          <div className='flex justify-between mt-12 gap-x-5 h-[400px]'>
-            <Trend origin='home' className={'w-full !h-full'} />
-            <Trend origin='home' className={'w-full !h-full'} />
-          </div>
-          <div className='mt-32' ref={ref}>
-            <Rank origin={'home'} />
-          </div>
-          <div className='flex w-full mt-12 gap-x-5 h-[489px]  ' ref={ref1}>
-            <div className='flex-1'>
-              <ContractRank origin='home' />
-            </div>
-            <div className='flex-1'>
-              <div className={`flex justify-between items-center h-[30px]`}>
-                <div className='font-PingFang font-semibold text-lg	'>
-                  {tr('defi_list')}
-                </div>
-                <Link href={`/contract/rank`}>
-                  <Image
-                    className='cursor-pointer'
-                    src={go}
-                    width={18}
-                    height={18}
-                    alt='go'
-                  />
-                </Link>
-              </div>
-              <DefiList origin='home' />
-            </div>
-          </div>
-        </BrowserView>
 
-        <MobileView>
-          <Defi/>
-        </MobileView>
+        </BrowserView>
       </div>
+
+      <MobileView>
+        <Defi/>
+      </MobileView>
     </div>
   );
 }
