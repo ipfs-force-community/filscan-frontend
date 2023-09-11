b0fb97bdf9505d0993e72854ad5bb205d097e5de
jinming.liu <mingming.lucky@foxmail.com>
2023-09-11 18:43:19 +0800

    feat: update

--- src/detail/list/index.tsx
+++ src/detail/list/index.tsx
@@ -73,7 +73,7 @@ export default ({
         )}
       </div>
 
-      <div className='card_shadow p-5 mt-5 min-h-[300px] table'>
+      <div className='card_shadow p-5 mt-5 min-h-[300px] border border_color rounded-xl table '>
         {activeTab === 'message_list' && (
           <MessageList accountId={accountId} methodName={method} />
         )}
