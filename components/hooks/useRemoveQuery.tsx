2616903794e2413d6fafb105b9783d37db1d0684
jinming.liu <mingming.lucky@foxmail.com>
2023-09-13 16:40:46 +0800

    feat: update

--- components/hooks/useRemoveQuery.tsx
+++ components/hooks/useRemoveQuery.tsx
@@ -6,23 +6,39 @@ const useRemoveQueryParam = () => {
   const router = useRouter();
 
   const removeQueryParam = (param: string | string[]) => {
+    const isHash = router.asPath.includes('#');
+    const hash = router.asPath.split('#')[1];
+
     if (Array.isArray(param)) {
       let restQueryParams = { ...router.query };
       param.forEach((param) => {
         const { [param]: removed, ...rest } = restQueryParams;
         restQueryParams = rest;
       });
+
       router.push({
         pathname: router.pathname,
         query: restQueryParams,
       });
     } else {
       const { [param]: removed, ...restQueryParams } = router.query;
-      router.push({
-        pathname: router.pathname,
-        query: restQueryParams,
-      });
+      if (hash) {
+        const filteredQueryParams = Object.fromEntries(
+          Object.entries(restQueryParams).filter(([key, value]) => typeof value === 'string')
+        );
+        // router.push({
+        //   pathname: router.pathname,
+        //   query: restQueryParams,
+        // }, `${router.pathname}?${new URLSearchParams(filteredQueryParams).toString()}${hash}`, { shallow: true });
+      } else {
+        router.push({
+          pathname: router.pathname ,
+          query: restQueryParams,
+        });
+      }
+
     }
+
   };
 
   return removeQueryParam;
