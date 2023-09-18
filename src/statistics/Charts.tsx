7230d753be97854cf55374ef6466b3b361ec3173
jinming.liu <mingming.lucky@foxmail.com>
2023-09-18 11:09:42 +0800

    feat: update text & account type

--- src/statistics/Charts.tsx
+++ src/statistics/Charts.tsx
@@ -86,37 +86,42 @@ function Overview() {
     setLegendData(newLegend)
   }
 
-  return <div className="card_shadow w-full border border_color rounded-[12px]">
-    <div className="flex flex-row border-b border_color">
-      <div className="w-2/3 h-[350px] py-5">
-        <EChart options={options}/>
-      </div>
-      <ul className="1/3 flex gap-y-2.5  flex-col justify-center">
-        {Object.keys(legendData).map((legendKey: any) => {
-          const legend = legendData[legendKey];
-          return <li key={legendKey} className='flex gap-x-2 items-center text-xs text_des cursor-pointer'
-            onClick={() => { handleLegend(legendKey)}}>
-            <span className='block w-4 h-4 rounded-full' style={{ background: legend.isShow ? legend?.color || "":'#d1d5db' }} />
-            <span>{ legend?.name||""}</span>
-          </li>
-        })}
-      </ul>
-
+  return <div>
+    <div className='flex items-center h-9 w-fit font-PingFang font-semibold text-lg pl-2.5 mb-4'>
+      { tr('charts_title') }
     </div>
-    <div className="p-10 text-xs font-DINPro-Medium text_des">
-      <ul className="border border_color rounded-[5px]">
-        {fil_charts.content.map((v,index) => {
-          return <li key={ index} className="flex border-b border_color w-full break-words min-h-[36px] flex items-center last:border-none">
-            <div style={{width:'20%'}} className="flex items-center h-full min-h-[36px] px-2.5  border-r border_color" >{tr(v.label)}</div>
-            <div style={{width:'25%'}} className="flex items-center h-full min-h-[36px]  px-2.5 border-r border_color">{index === 0 ? tr(v.value): v.value}</div>
-            <div style={{width:'40%'}} className="px-2.5" >{ tr(v.description)}</div>
+    <div className="card_shadow w-full border border_color rounded-[12px]">
 
-          </li>
-        })}
-      </ul>
+      <div className="flex flex-row border-b border_color">
+        <div className="w-2/3 h-[350px] py-5">
+          <EChart options={options}/>
+        </div>
+        <ul className="1/3 flex gap-y-2.5  flex-col justify-center">
+          {Object.keys(legendData).map((legendKey: any) => {
+            const legend = legendData[legendKey];
+            return <li key={legendKey} className='flex gap-x-2 items-center text-xs text_des cursor-pointer'
+              onClick={() => { handleLegend(legendKey)}}>
+              <span className='block w-4 h-4 rounded-full' style={{ background: legend.isShow ? legend?.color || "":'#d1d5db' }} />
+              <span>{ legend?.name||""}</span>
+            </li>
+          })}
+        </ul>
 
+      </div>
+      <div className="p-10 text-xs font-DINPro-Medium text_des">
+        <ul className="border border_color rounded-[5px]">
+          {fil_charts.content.map((v,index) => {
+            return <li key={ index} className="flex border-b border_color w-full break-words min-h-[36px] flex items-center last:border-none">
+              <div style={{width:'20%'}} className="flex items-center h-full min-h-[36px] px-2.5  border-r border_color" >{tr(v.label)}</div>
+              <div style={{width:'25%'}} className="flex items-center h-full min-h-[36px]  px-2.5 border-r border_color">{index === 0 ? tr(v.value): v.value}</div>
+              <div style={{width:'40%'}} className="px-2.5" >{ tr(v.description)}</div>
+
+            </li>
+          })}
+        </ul>
+
+      </div>
     </div>
   </div>
-
 }
 export default Overview;
