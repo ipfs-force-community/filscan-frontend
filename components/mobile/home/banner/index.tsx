fce402473ab504ed5b5c34998b9f2a8cd6e9c031
abu0306 <2261983469@qq.com>
2023-09-25 11:05:13 +0800

    feat: add home banner

--- components/mobile/home/banner/index.tsx
+++ components/mobile/home/banner/index.tsx
@@ -1,13 +1,44 @@
 import styles from './style.module.scss';
 import { observer } from 'mobx-react';
 import { Carousel } from 'antd';
+import useAxiosData from '@/store/useAxiosData';
+import { useFilscanStore } from '@/store/FilscanStore';
+import { useEffect, useRef, useState } from 'react';
+import useWindow from '@/components/hooks/useWindown';
+import { apiUrl } from '@/contents/apiUrl';
+import { Image } from 'antd'
 
 const Banner = (props: any)=> {
+
+  const { axiosData } = useAxiosData()
+  const { theme, lang } = useFilscanStore();
+  const carousel = useRef<any>(null)
+  const [data, setData] = useState([])
+  const {isMobile} = useWindow()
+  useEffect(() => {
+    loadBanner()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lang,isMobile])
+
+  const loadBanner = async() => {
+    const result:any =await axiosData(apiUrl.home_banner, {
+      category: 'moblie_home',
+      language:lang ||'zh'
+    })
+    setData(result?.items||[])
+  }
+
   return <div className={styles.banner}>
     <Carousel>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+      {data?.map((item: any,index) => {
+        return <div key={ index} onClick={() => {
+          if (item.link) {
+            window.open(item.link)
+          }
+        }}>
+          <Image preview={false} src={item.url} alt='' width='100%' className="cursor-pointer object-cover carousel-image rounded-[6px]"/>
+        </div>
+      })}
     </Carousel>
   </div>;
 }
