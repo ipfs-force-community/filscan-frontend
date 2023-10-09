a21b04eed8f61ffa4077b5479b806aba42d78584
abu0306 <2261983469@qq.com>
2023-10-09 11:39:19 +0800

    fix: fix the deal deal style

--- pages/deal/[id].tsx
+++ pages/deal/[id].tsx
@@ -93,18 +93,18 @@ export default () => {
           </div>
         </div>
 
-        <div className={classNames(styles.space,`flex items-center justify-center flex-col gap-y-4 py-2.5 h-[114px] rounded-[5px] border border-color`)}>
+        <div className={classNames(styles.space,`flex items-center justify-center flex-col py-2.5 rounded-[5px] border border-color`)}>
           <span className="flex items-center gap-x-1 text_color font-DINPro-Medium">
             <Cloud width={14} height={14} />
             { data?.piece_size && unitConversion( data.piece_size)}
           </span>
-          <span>
+          <span className={classNames(styles.time)}>
             {formatDateTime(data?.service_start_time)}
             { tr(deal_list.content.time)}
             { formatDateTime(data?.end_time)}
           </span>
           <span>
-            {tr(deal_list.content?.cash)}:
+            <span className={styles.cash}>{tr(deal_list.content?.cash)}:</span>
             <span className="text_color font-DINPro-Medium">
               { data?.storage_price_per_epoch&&formatFilNum(data?.storage_price_per_epoch)}
             </span>
