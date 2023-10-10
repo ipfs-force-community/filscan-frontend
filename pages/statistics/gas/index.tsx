25b08d9a7928e4ac5f6a20299b89cd38caa7a802
abu0306 <2261983469@qq.com>
2023-10-10 10:48:58 +0800

    fix:  gas page echart layout

--- pages/statistics/gas/index.tsx
+++ pages/statistics/gas/index.tsx
@@ -24,14 +24,14 @@ export default () => {
       <div className='flex flex-col text-lg font-medium gap-y-2.5 mb-4 mx-2.5 gas-title'>
         <span>{tr('gas')}</span>
       </div>
-      <div className='w-full h-[348px] card_shadow pr-5 pt-5 pb-2.5 !overflow-hidden border  border_color rounded-xl'>
+      <div className={classNames('w-full h-[348px] card_shadow pr-5 pt-5 pb-2.5 !overflow-hidden border  border_color rounded-xl',styles.gas)}>
         <Gas />
       </div>
       <>
         <div className='flex flex-col text-lg font-medium gap-y-2.5 my-5'>
           <span>{tr('gas_24')}</span>
         </div>
-        <div className='border rounded-xl p-5 card_shadow border_color flex items-center gas-table'>
+        <div className={classNames('border rounded-xl p-5 card_shadow border_color flex items-center gas-table')}>
           <Table
             data={(GasData && GasData.items) || []}
             columns={columns}
