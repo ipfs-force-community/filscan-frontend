5f93fd5ac73a7becb29d089563f42d0b2ae6abff
jinming.liu <mingming.lucky@foxmail.com>
2023-10-25 13:17:16 +0800

    feat: fevm charts

--- src/statistics/Meta.tsx
+++ src/statistics/Meta.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Translation } from '@/components/hooks/Translation'
 import useInterval from '@/components/hooks/useInterval'
 import { EvmContractSummary, apiUrl, metaOther } from '@/contents/apiUrl'
@@ -9,18 +8,6 @@ import { getSvgIcon } from '@/svgsIcon'
 import { data } from 'autoprefixer'
 import classNames from 'classnames'
 import { useEffect, useState } from 'react'
-=======
-import { Translation } from "@/components/hooks/Translation";
-import useInterval from "@/components/hooks/useInterval";
-import { EvmContractSummary, apiUrl, metaOther } from "@/contents/apiUrl";
-import { meta_list } from "@/contents/home"
-import Tooltip from "@/packages/tooltip";
-import useAxiosData from "@/store/useAxiosData";
-import { getSvgIcon } from "@/svgsIcon";
-import { data } from "autoprefixer";
-import classNames from "classnames";
-import { useEffect, useState } from "react";
->>>>>>> d3b73c0f (fix: nav cw)
 import styles from './Meta.module.scss'
 export default () => {
   const { tr } = Translation({ ns: 'home' })
@@ -29,7 +16,6 @@ export default () => {
   const [otherData, setOtherData] = useState({})
   const [contractData, setContractData] = useState<Record<string, any>>()
 
-<<<<<<< HEAD
   useInterval(
     () => {
       load()
@@ -37,12 +23,6 @@ export default () => {
     },
     5 * 60 * 1000,
   )
-=======
-  useInterval(() => {
-    load();
-    loadOther();
-  }, 5*60*1000);
->>>>>>> d3b73c0f (fix: nav cw)
 
   const load = async () => {
     const data: any = await axiosData(apiUrl.home_meta)
@@ -52,7 +32,6 @@ export default () => {
   }
 
   const loadOther = async () => {
-<<<<<<< HEAD
     const otherData: any = await axiosData(metaOther)
     setOtherData(otherData || {})
   }
@@ -100,33 +79,3 @@ export default () => {
     </ul>
   )
 }
-=======
-    const otherData: any = await axiosData(metaOther);
-    setOtherData(otherData || {});
-  }
-
-  return <ul className={classNames("flex flex-wrap gap-5 card_shadow py-5 px-6 border border_color rounded-[12px] mt-[46px]",styles.wrap,styles['wrap-reset'])}>
-    {meta_list.map((item: any, index: number) => {
-      const { title,tip, render, dataIndex } = item;
-      const newData = {...contractData,...data,...otherData}
-      const value = newData[dataIndex];
-      let renderDom = render ? render(value, newData, tr) : value;
-      return <li className={classNames("flex flex-col items-center w-[210px] py-4 gap-y-1 border border_color rounded-[5px]",styles.item,styles['item-reset'])} key={index}>
-        <div className="font-medium font-DINPro-Bold text-base">{renderDom}</div>
-        {tip ?
-          <>
-            <Tooltip context={tr(tip)} icon={ false}>
-              <div className="text_des flex items-center gap-x-1">
-                <span>
-                  {tr(item.title)}
-                </span>
-                { getSvgIcon('tip')}
-              </div>
-            </Tooltip>
-          </>: <div className="text_des ">{tr(item.title)}</div>
-        }
-      </li>
-    })}
-  </ul>
-}
->>>>>>> d3b73c0f (fix: nav cw)
