536d0ddd02efecc8921ca5081727edb353bbb979
jinming.liu <mingming.lucky@foxmail.com>
2023-10-25 13:12:06 +0800

    fix: nav cw

--- contents/nav.ts
+++ contents/nav.ts
@@ -24,7 +24,7 @@ const navMenu: Array<Menu_Info | any> = [
       },
       { key: "statistics_gas", link: "/statistics/gas" },
       { key: "statistics_charts", link: "/statistics/charts" },
-      { key: "cw", link: "/cw" },
+      // { key: "cw", link: "/cw" },
     ],
   },
   {
