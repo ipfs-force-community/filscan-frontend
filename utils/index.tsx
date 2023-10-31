fd5d4c7f3d625c0e1015e9f678d3faa23843e1b7
jinming.liu <mingming.lucky@foxmail.com>
2023-10-31 09:42:34 +0800

    fix: fixed axiosWindow

--- utils/index.tsx
+++ utils/index.tsx
@@ -405,3 +405,17 @@ export const isMobile = ()=>{
     return false
   }
 }
+
+// save to storage
+export const saveToStorage = (key:string, value:any) => {
+  if(typeof window !== 'undefined') {
+    return window.localStorage.setItem(key, value);
+  }
+}
+
+// get from storage
+export const getFromStorage = (key:string) => {
+  if (typeof window !== 'undefined') {
+    return window.localStorage.getItem(key);
+  }
+}
