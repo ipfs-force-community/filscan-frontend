6e1c500bf02d0856970306095cbb7696adb68e21
abu0306 <2261983469@qq.com>
2023-12-05 17:08:44 +0800

    feat: overview module style

--- src/account/overview.tsx
+++ src/account/overview.tsx
@@ -11,6 +11,11 @@ import { useEffect, useMemo, useState } from 'react'
 import Groups from './Groups'
 import overviewStore from '@/store/modules/account/overview'
 import { observer } from 'mobx-react'
+import styles from './overview.module.scss'
+import { BrowserView, MobileView } from '@/components/device-detect'
+import classNames from 'classnames'
+import MTable from '@/packages/mobile/table'
+import useWindow from '@/components/hooks/useWindown'
 
 interface Props {
   selectedKey: string
@@ -21,12 +26,16 @@ export default observer((props: Props) => {
   const { tr } = Translation({ ns: 'account' })
   const [active, setActive] = useState<string>('-1')
   const { overviewData = {}, loading = true } = overviewStore
+  const { isMobile } = useWindow()
 
   const columns = useMemo(() => {
     return overview.columns(tr).map((item) => {
+      if (isMobile) {
+        item.fixed = 'auto'
+      }
       return { ...item, title: tr(item.title) }
     })
-  }, [tr])
+  }, [tr, isMobile])
 
   useEffect(() => {
     load()
@@ -42,66 +51,139 @@ export default observer((props: Props) => {
   }
 
   return (
-    <>
-      <div className="flex items-center justify-between">
-        <div className="flex  flex-col">
-          <span className="w-full font-HarmonyOS text-lg font-semibold	">
-            {tr(selectedKey)}
-          </span>
-          <span className="text_des text-xs">
-            <span>{tr('last_time')}</span>
-            <span className="ml-2">
-              {formatDateTime(overviewData?.epoch_time, 'YYYY/MM/DD HH:mm')}
+    <div className={styles.wrap}>
+      <BrowserView>
+        <div className="flex items-center justify-between">
+          <div className="flex  flex-col">
+            <span className="w-full font-HarmonyOS text-lg font-semibold	">
+              {tr(selectedKey)}
             </span>
-          </span>
+            <span className="text_des text-xs">
+              <span>{tr('last_time')}</span>
+              <span className="ml-2">
+                {formatDateTime(overviewData?.epoch_time, 'YYYY/MM/DD HH:mm')}
+              </span>
+            </span>
+          </div>
+          <div className="flex gap-x-2.5">
+            <Groups
+              selectGroup={active}
+              onChange={(value: string) => {
+                load(value)
+                setActive(value)
+              }}
+            />
+            <ExportExcel
+              columns={columns}
+              data={overviewData?.miner_info_detail_list || []}
+              fileName={'expired'}
+            />
+          </div>
         </div>
-        <div className="flex gap-x-2.5">
-          <Groups
-            selectGroup={active}
-            onChange={(value: string) => {
-              load(value)
-              setActive(value)
-            }}
-          />
-          <ExportExcel
-            columns={columns}
+      </BrowserView>
+      <MobileView>
+        <div className={styles['title-wrap']}>
+          <div className="flex  flex-col">
+            <span className={styles.title}>{tr(selectedKey)}</span>
+            <span className={styles.time}>
+              <span>{tr('last_time')}</span>
+              <span className="ml-2">
+                {formatDateTime(overviewData?.epoch_time, 'YYYY/MM/DD HH:mm')}
+              </span>
+            </span>
+          </div>
+          <div className={styles.group}>
+            <Groups
+              selectGroup={active}
+              onChange={(value: string) => {
+                load(value)
+                setActive(value)
+              }}
+            />
+          </div>
+        </div>
+      </MobileView>
+      <BrowserView>
+        <div className="mt-5 flex flex-col gap-y-5">
+          {overview.headerList.map((itemArray, index) => {
+            return (
+              <ul
+                className={`flex min-h-[133px] flex-wrap gap-x-5`}
+                key={index}
+              >
+                {itemArray.map((item) => {
+                  const { icon, dataIndex, render } = item
+                  const showValue = render(
+                    overviewData && overviewData[dataIndex],
+                    overviewData,
+                    tr,
+                  )
+                  return (
+                    <li
+                      key={item.dataIndex}
+                      className="card_shadow border_color flex flex-1 items-start justify-between  rounded-xl border p-6"
+                    >
+                      {showValue}
+                      {icon && <span>{getSvgIcon(icon)}</span>}
+                    </li>
+                  )
+                })}
+              </ul>
+            )
+          })}
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className={styles.overviewWrap}>
+          {overview.headerList
+            .reduce((prev, itemArray, index) => {
+              return prev.concat(itemArray)
+            }, [])
+            .map((item, index) => {
+              const { icon, dataIndex, render } = item
+              const showValue = render(
+                overviewData && overviewData[dataIndex],
+                overviewData,
+                tr,
+              )
+              return (
+                <li
+                  key={item.dataIndex}
+                  className={classNames(
+                    'card_shadow border_color flex flex-1 items-start justify-between  rounded-xl border p-6',
+                    styles[`item-${index}`],
+                    styles.item,
+                  )}
+                >
+                  {showValue}
+                  {icon && <span>{getSvgIcon(icon)}</span>}
+                </li>
+              )
+            })}
+        </div>
+      </MobileView>
+      <div
+        className={classNames(
+          'card_shadow border_color mt-5 overflow-auto rounded-xl border p-4',
+          styles['table-wrap'],
+        )}
+      >
+        <BrowserView>
+          <Table
             data={overviewData?.miner_info_detail_list || []}
-            fileName={'expired'}
+            columns={columns}
+            loading={loading}
           />
-        </div>
+        </BrowserView>
+        <MobileView>
+          <MTable
+            scroll={{ x: 'max-content' }}
+            dataSource={overviewData?.miner_info_detail_list || []}
+            columns={columns as any}
+            loading={loading}
+          />
+        </MobileView>
       </div>
-      <div className="mt-5 flex flex-col gap-y-5">
-        {overview.headerList.map((itemArray, index) => {
-          return (
-            <ul className={`flex min-h-[133px] flex-wrap gap-x-5`} key={index}>
-              {itemArray.map((item) => {
-                const { icon, dataIndex, render } = item
-                const showValue = render(
-                  overviewData && overviewData[dataIndex],
-                  overviewData,
-                  tr,
-                )
-                return (
-                  <li
-                    key={item.dataIndex}
-                    className="card_shadow border_color flex flex-1 items-start justify-between  rounded-xl border p-6"
-                  >
-                    {showValue}
-                    {icon && <span>{getSvgIcon(icon)}</span>}
-                  </li>
-                )
-              })}
-            </ul>
-          )
-        })}
-      </div>
-      <div className="card_shadow border_color mt-5 overflow-auto rounded-xl border p-4">
-        <Table
-          data={overviewData?.miner_info_detail_list || []}
-          columns={columns}
-          loading={loading}
-        />
-      </div>
-    </>
+    </div>
   )
 })
